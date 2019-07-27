8d7777f6fd9ef87dd21a85fc60870981a349f165
longxionga <605142472@qq.com>
2019-07-27 21:33:38 +0800

    谈谈

---
+++ src/main/java/com/acl/test/test22.java
@@ -0,0 +1,19 @@
+package com.acl.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class test22 {
+    public static void main(String[] args) {
+        String  s = "00003106681906043992518";
+        long a = Long.valueOf(s);
+        System.out.println(a);
+        Map<String ,Object> m = new HashMap<>();
+        m.put("s",12);
+        int sm = Integer.valueOf(m.get("s").toString());
+        m.put("s",sm+3);
+        int smw = Integer.valueOf(m.get("s").toString());
+        int qwe = Integer.valueOf(m.get("s").toString());
+        int aaa = Integer.valueOf(m.get("s").toString());
+    }
+}
